1fa8aed4b7ddbf9f451b0605ffb16652dc8b3da4
Markus Røyset <maroider@protonmail.com>
2021-01-02 05:43:45 +0100

    On Windows, address compilation errors for i686

--- src/platform_impl/windows/mod.rs
+++ src/platform_impl/windows/mod.rs
@@ -1,5 +1,7 @@
+use std::convert::TryInto;
+
 use winapi::{
-    shared::windef::HWND__,
+    shared::{basetsd::LONG_PTR, windef::HWND},
     um::winuser::{
         GetParent, GetWindow, GetWindowLongPtrW, SetForegroundWindow, SetLayeredWindowAttributes,
         SetWindowLongPtrW, GWL_EXSTYLE, GW_HWNDNEXT, LWA_ALPHA,
@@ -13,21 +15,15 @@ const WS_EX_LAYERED: isize = 0x80000;
 pub fn make_window_overlay(window: &Window, opacity: u8) {
     window.set_always_on_top(true);
 
-    let hwnd = window.hwnd() as *mut HWND__;
+    let hwnd = window.hwnd() as HWND;
 
     let window_styles: isize = match unsafe { GetWindowLongPtrW(hwnd, GWL_EXSTYLE) } {
         0 => panic!("GetWindowLongPtrW returned 0"),
-        ptr => ptr,
+        ptr => ptr.try_into().unwrap(),
     };
+    let window_styles: LONG_PTR = window_styles | WS_EX_TRANSPARENT | WS_EX_LAYERED;
 
-    if unsafe {
-        SetWindowLongPtrW(
-            hwnd,
-            GWL_EXSTYLE,
-            window_styles | WS_EX_TRANSPARENT | WS_EX_LAYERED,
-        )
-    } == 0
-    {
+    if unsafe { SetWindowLongPtrW(hwnd, GWL_EXSTYLE, window_styles.try_into().unwrap()) } == 0 {
         panic!("SetWindowLongPtr returned 0");
     }
 
@@ -39,28 +35,22 @@ pub fn make_window_overlay(window: &Window, opacity: u8) {
 pub fn make_window_overlay_clickable(window: &Window, opacity: u8) {
     window.set_always_on_top(false);
 
-    let hwnd = window.hwnd() as *mut HWND__;
+    let hwnd = window.hwnd().cast() as HWND;
 
     let window_styles: isize = match unsafe { GetWindowLongPtrW(hwnd, GWL_EXSTYLE) } {
         0 => panic!("GetWindowLongPtrW returned 0"),
-        ptr => ptr,
+        ptr => ptr.try_into().unwrap(),
     };
+    let window_styles = window_styles & !WS_EX_TRANSPARENT | WS_EX_LAYERED;
 
-    if unsafe {
-        SetWindowLongPtrW(
-            hwnd,
-            GWL_EXSTYLE,
-            window_styles & !WS_EX_TRANSPARENT | WS_EX_LAYERED,
-        )
-    } == 0
-    {
+    if unsafe { SetWindowLongPtrW(hwnd, GWL_EXSTYLE, window_styles.try_into().unwrap()) } == 0 {
         panic!("SetWindowLongPtr returned 0");
     }
 
     set_window_overlay_opacity(window, opacity);
 }
 
-unsafe fn make_last_active_window_active(hwnd: *mut HWND__) {
+unsafe fn make_last_active_window_active(hwnd: HWND) {
     let mut last_window = GetWindow(hwnd, GW_HWNDNEXT);
     if last_window.is_null() {
         panic!("GetWindow returned 0");
@@ -83,7 +73,7 @@ unsafe fn make_last_active_window_active(hwnd: *mut HWND__) {
 }
 
 pub fn set_window_overlay_opacity(window: &Window, opacity: u8) {
-    let hwnd = window.hwnd() as *mut HWND__;
+    let hwnd = window.hwnd() as HWND;
 
     if unsafe { SetLayeredWindowAttributes(hwnd, 0, opacity, LWA_ALPHA) } == 0 {
         panic!("SetLayeredWindowAttributes returned 0");
